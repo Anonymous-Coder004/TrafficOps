a76ad8dda1f0abcd86e11398a8f566dd4b493899
Yash_Srivastava <yash13sri@gmail.com>
2026-07-23 03:24:34 +0530

    feat:marker

--- frontend/src/components/maps/markers/SelectedLocationMarker.jsx
+++ frontend/src/components/maps/markers/SelectedLocationMarker.jsx
@@ -1,5 +1,5 @@
 import {Marker} from "react-leaflet"
-
+import { selectedLocationIcon } from "../utils/icons"
 export default function SelectedLocationMarker({
     location
 }) {
@@ -14,6 +14,7 @@ export default function SelectedLocationMarker({
                 location.lat,
                 location.lng
             ]}
+            icon={selectedLocationIcon}
         />
     )
 }
--- frontend/src/components/maps/utils/icons.js
+++ frontend/src/components/maps/utils/icons.js
@@ -17,3 +17,12 @@ export const incidentIcon = new L.Icon({
     popupAnchor: [1, -34],
     shadowSize: [41, 41],
 });
+
+export const selectedLocationIcon = new L.Icon({
+    iconUrl: "/markers/marker-icon-blue.png",
+    shadowUrl: "/markers/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+});
